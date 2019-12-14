d94c8fb01a8277f77f1aae7db7d1845990056bc2
hannaGirdharimal <hanna2000.michelle@gmail.com>
2019-12-14 23:55:10 +0530

    Changes

--- SpreeMart/Scripts/Menu.js
+++ SpreeMart/Scripts/Menu.js
@@ -1,9 +1,15 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "270px";
-  }
+}
   
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
+/* Set the width of the side navigation to 0 */
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-  }
+}
+
+// $( function() {
+//   $( "#progressbar" ).progressbar({
+//     value: 37
+//   });
+// } );
