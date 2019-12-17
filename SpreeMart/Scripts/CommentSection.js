4ab841f1a5b2309a53a180b983f9796329eca51b
Ashandis <ashan246@gmail.com>
2019-12-17 20:27:03 +0530

    comments

---
+++ SpreeMart/Scripts/CommentSection.js
@@ -0,0 +1,38 @@
+function handleSubmit() {
+    const comment = document.getElementById("comment-box").value;
+    document.getElementById("comment-box").value = "";
+    let currentComment = localStorage.getItem("comment");
+    let newComment = "";
+    if (!currentComment) {
+        newComment = `${comment}`;
+    } else {
+        newComment = `${currentComment},${comment}`;
+    }
+
+    localStorage.setItem("comment", newComment);
+
+    const commentArray = newComment.split(",");
+    const commentHTML = commentArray.map((cmt) => {
+        return `<div class="ui-grid-a" style="margin-bottom: 10px;">
+       <div class="ui-block-a comment-pic-block">
+           <img id="comment-pic" src="Images/jan-profile-circle.png">
+           <div class="commenter-name">Janet Miller</div>
+       </div>
+       <div class="ui-block-b comment-block">
+           <div class="comment">
+               ${cmt}
+           </div>
+           <div class="rating-2">
+               <img id="rating-star" src="Icons/star-border.png">
+               <img id="rating-star" src="Icons/star-border.png">
+               <img id="rating-star" src="Icons/star-border.png">
+               <img id="rating-star" src="Icons/star-border.png">
+               <img id="rating-star" src="Icons/star-border.png">
+           </div>
+       </div>
+   </div>`
+    });
+
+    const commentList = document.getElementById("comment-list");
+    commentList.innerHTML = commentHTML;
+}
