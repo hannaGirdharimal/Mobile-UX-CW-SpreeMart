619a491fca4f336f002b29e67081a9c4e1dda8f3
hannaGirdharimal <hanna2000.michelle@gmail.com>
2019-12-18 01:34:34 +0530

    Finished search and filter

--- SpreeMart/Scripts/Menu.js
+++ SpreeMart/Scripts/Menu.js
@@ -1,32 +1,37 @@
-/* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "270px";
 }
   
-/* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-// $( function() {
-//   $( "#progressbar" ).progressbar({
-//     value: 37
-//   });
-// } );
-
-
 filterSelection("all")
 function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("card");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    var x, i, button;
+    x = document.getElementsByClassName("card");
+    buttonText = document.getElementsByClassName("dropdown-toggle-text");
+    if (c == "all"){
+        c = "";
+        buttonText[0].innerHTML= "All";
+    } else
+    if(c == "best"){
+      buttonText[0].innerHTML= "Best Sellers";
+    }else if(c == "trending"){
+      buttonText[0].innerHTML= "Trending";
+    }
+    for (i = 0; i < x.length; i++) {
+      RemoveClass(x[i], "show-card");
+      RemoveClass(x[i], "hide-card");
+      if (x[i].className.indexOf(c) > -1){
+        AddClass(x[i], "show-card");
+      }else{
+        AddClass(x[i], "hide-card");
+      }
+    }
   }
-}
 
-function w3AddClass(element, name) {
+function AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -35,7 +40,7 @@ function w3AddClass(element, name) {
   }
 }
 
-function w3RemoveClass(element, name) {
+function RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -47,14 +52,12 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+function changeImage() {
+    var image = document.getElementById('heart');
+    if (image.src.match("Icons/unfilled-heart.png")) {
+        image.src = "Icons/heart.png";
+    }
+    else {
+        image.src = "Icons/unfilled-heart.png";
+    }
 }
-
