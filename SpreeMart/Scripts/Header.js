ea8a348fa5246b12a1dce37f9e98b654c4dddcae
Ashandis <ashan246@gmail.com>
2019-12-19 00:14:27 +0530

    link

---
+++ SpreeMart/Scripts/Header.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+    // $("[data-role='header'], [data-role='footer']").toolbar();
+
+    $('#item-list1').click(() => {
+        window.location.href = 'Products page-ipad.html';
+    });
+
+    $('#item-list2').click(() => {
+        window.location.href = 'Products page-ipad.html';
+    });
+
+    $('#item-list3').click(() => {
+        window.location.href = 'Products page-ipad.html';
+    });
+
+    $('#item-promotion').click(() => {
+        window.location.href = 'Promotions page-iPad.html';
+    });
+
+    $('#item-support').click(() => {
+        window.location.href = 'support-ipad.html';
+    });
+
+    $('#item-list3').click(() => {
+        window.location.href = 'Products page-ipad.html';
+    });
+
+});
