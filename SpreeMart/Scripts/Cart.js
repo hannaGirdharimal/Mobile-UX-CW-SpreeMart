7f4ae089b7c53c35eecab0889e3414459d60a2a4
Ashandis <ashan246@gmail.com>
2019-12-19 11:06:30 +0530

    babu

--- SpreeMart/Scripts/Cart.js
+++ SpreeMart/Scripts/Cart.js
@@ -1,91 +1,91 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     let addtocartlist = localStorage.getItem("addtocartList");
 
     const AddToCartIDArray = addtocartlist.split(",");
-    
-    const noOfItems=AddToCartIDArray.length;
 
-    document.getElementById('delete-txt').innerHTML='('+noOfItems+')'+'Items';
+    const noOfItems = AddToCartIDArray.length;
 
-    for(cartItem of AddToCartIDArray ){
+    document.getElementById('delete-txt').innerHTML = '(' + noOfItems + ')' + 'Items';
+
+    for (cartItem of AddToCartIDArray) {
         var cartProduct;
         console.log(cartItem);
-         for(var i=0;i<Products.length;i++){
-             if(Products[i].product_id===cartItem){              
-                 const CartHTML='<div class="ui-grid-b card">'+
-                 '<div class="ui-block-a left-wrapper">'+
-                   '<div class=" ui-checkbox">'+
-                   '<input type="checkbox"  class="product-check" name="checkbox-mini-0" id="checkbox-select-all" data-mini="true">'+
-                   '</div>'+
-                 '</div>'+
-                 '<div class="ui-block-b middle-wrapper">'+
-                   '<img id="nutella" src="'+Products[i].img_url+'">'+
-                 '</div>'+
-                 '<div class="ui-block-c right-wrapper">'+
-                   '<span class="producttitle">'+ Products[i].title+'</span>'+
-                   '<img id="share-2" src="Icons/delete.png">'+
-                    '<br>'+
-                   '<span class="price">'+ Products[i].price+'</span>'+
-                   '<span class="rating1">'+
-                     '<img id="rating-star1" src="Icons/rating star.png">'+
-                     '<img id="rating-star1" src="Icons/rating star.png">'+
-                     '<img id="rating-star1" src="Icons/rating star.png">'+
-                     '<img id="rating-star1" src="Icons/rating star.png">'+
-                     '<img id="rating-star1" src="Icons/rating star half-filled.png">'+
-                    '</span>'+
-                 '</div>'+
-               '</div>'
-                document.getElementsByClassName('card-area')[0].innerHTML+=CartHTML;
-                 break;
-             } 
-         }
-         console.log(cartProduct);
+        for (var i = 0; i < Products.length; i++) {
+            if (Products[i].product_id === cartItem) {
+                const CartHTML = '<div class="ui-grid-b card">' +
+                    '<div class="ui-block-a left-wrapper">' +
+                    '<div class=" ui-checkbox">' +
+                    '<input type="checkbox"  class="product-check" name="checkbox-mini-0" id="checkbox-select-all" data-mini="true">' +
+                    '</div>' +
+                    '</div>' +
+                    '<div class="ui-block-b middle-wrapper">' +
+                    '<img id="nutella" src="' + Products[i].img_url + '">' +
+                    '</div>' +
+                    '<div class="ui-block-c right-wrapper">' +
+                    '<span class="producttitle">' + Products[i].title + '</span>' +
+                    '<img id="share-2" src="Icons/delete.png">' +
+                    '<br>' +
+                    '<span class="price">' + Products[i].price + '</span>' +
+                    '<span class="rating1">' +
+                    '<img id="rating-star1" src="Icons/rating star.png">' +
+                    '<img id="rating-star1" src="Icons/rating star.png">' +
+                    '<img id="rating-star1" src="Icons/rating star.png">' +
+                    '<img id="rating-star1" src="Icons/rating star.png">' +
+                    '<img id="rating-star1" src="Icons/rating star half-filled.png">' +
+                    '</span>' +
+                    '</div>' +
+                    '</div>'
+                document.getElementsByClassName('card-area')[0].innerHTML += CartHTML;
+                break;
+            }
+        }
+        console.log(cartProduct);
     }
 
-    var subtotal=0;
-    var NetTotal=0;
+    var subtotal = 0;
+    var NetTotal = 0;
 
 
-    $('#delete-icon').click(function(){
-      localStorage.clear('addtocartList');
-      location.reload();
+    $('#delete-icon').click(function() {
+        localStorage.clear('addtocartList');
+        location.reload();
     });
-    
-    var checkedItemsList=[];
 
-    $(".product-check").change(function () {
-       
-        if(this.checked) {
-            var itemprice=parseInt($('.price').eq($('.product-check').index(this)).text());
+    var checkedItemsList = [];
+
+    $(".product-check").change(function() {
+
+        if (this.checked) {
+            var itemprice = parseInt($('.price').eq($('.product-check').index(this)).text());
             subtotal += itemprice;
             checkedItemsList.push(itemprice);
-            NetTotal=subtotal;
-            document.getElementsByClassName('subprice')[0].innerText='Rs.'+subtotal;
-            document.getElementsByClassName('subprice3')[0].innerHTML='Rs. '+NetTotal;
+            NetTotal = subtotal;
+            document.getElementsByClassName('subprice')[0].innerText = 'Rs.' + subtotal;
+            document.getElementsByClassName('subprice3')[0].innerHTML = 'Rs. ' + NetTotal;
 
         }
 
-        if(!this.checked) {
-          var itemprice=parseInt($('.price').eq($('.product-check').index(this)).text());
+        if (!this.checked) {
+            var itemprice = parseInt($('.price').eq($('.product-check').index(this)).text());
             subtotal -= itemprice;
-            NetTotal=subtotal;
-           
-            for( var i = 0; i < checkedItemsList.length; i++){ 
-              if ( checkedItemsList[i] === itemprice) {
-                 checkedItemsList.splice(i, 1); 
-              }
-           }
-            document.getElementsByClassName('subprice')[0].innerHTML='Rs. '+subtotal;
-            document.getElementsByClassName('subprice3')[0].innerHTML='Rs. '+NetTotal;
+            NetTotal = subtotal;
+
+            for (var i = 0; i < checkedItemsList.length; i++) {
+                if (checkedItemsList[i] === itemprice) {
+                    checkedItemsList.splice(i, 1);
+                }
+            }
+            document.getElementsByClassName('subprice')[0].innerHTML = 'Rs. ' + subtotal;
+            document.getElementsByClassName('subprice3')[0].innerHTML = 'Rs. ' + NetTotal;
         }
     });
 
-    document.getElementById("BuyNow1").addEventListener("click", function () {
-      localStorage.setItem("PriceList",checkedItemsList);
-      localStorage.setItem("Subtotal", subtotal);
-      console.log(subtotal);
-      window.location.href = 'Checkout-iPhone.html';
-    }); 
+    document.getElementById("BuyNow1").addEventListener("click", function() {
+        localStorage.setItem("PriceList", checkedItemsList);
+        localStorage.setItem("Subtotal", subtotal);
+        console.log(subtotal);
+        window.location.href = 'Checkout-iPhone.html';
+    });
 
 });
---
+++ SpreeMart/Scripts/header-iphone.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+    // $("[data-role='header'], [data-role='footer']").toolbar();
+
+    $('#item-list1').click(() => {
+        window.location.href = 'Product-iphone.html';
+    });
+
+    $('#item-list2').click(() => {
+        window.location.href = 'Product-iphone.htmll';
+    });
+
+    $('#item-list3').click(() => {
+        window.location.href = 'Product-iphone.html';
+    });
+
+    $('#item-promotion').click(() => {
+        window.location.href = 'Promotions page-iPad.html';
+    });
+
+    $('#item-support').click(() => {
+        window.location.href = 'support-ipad.html';
+    });
+
+    $('#logo').click(() => {
+        window.location.href = 'Landing page-ipad.html';
+    });
+
+    $('#cart').click(() => {
+        window.location.href = 'Cart-ipad.html';
+    });
+
+    $('#dropdown-profile').click(() => {
+        window.location.href = 'Profile page-ipad.html';
+    });
+
+    $('#dropdown-fav').click(() => {
+        window.location.href = 'Favourites-ipad.html';
+    });
+
+    $('#dropdown-logout').click(() => {
+        window.location.href = 'login-ipad.html';
+    });
+
+
+
+});
